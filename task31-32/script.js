cb2c858038182aaa69baa1e8c6f7c18c4b955616
= <=>
2026-09-14 16:30:08 +0300

    add code

---
+++ task31-32/script.js
@@ -0,0 +1,24 @@
+// console.log(100 == "100"); // true
+// console.log(100 < 1000); // true
+// console.log(110 > 100 > 10 > 20); // true
+// console.log(-10 == "-10"); // true
+// console.log(? -50 ? ? +"-40"); // true
+// console.log(? 10 ? ? -"-40"); // true
+// console.log(? "10" ? ? 10); // true
+// console.log(? 20 ? false); // true
+
+console.log(100 == "100"); // true
+
+console.log(1000 == 1000); // true
+
+console.log(110 > 100 > 10 < 20); // true
+
+console.log(-10 == "-10"); // true
+
+console.log(-50 == +"-50"); // true
+
+console.log(10 == -"-10"); // true
+
+console.log("10" == +"10"); // true
+
+console.log(20 != false); // true
