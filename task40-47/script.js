e5609419aec86048b4ddd152405f75e360eb2160
= <=>
2026-09-23 09:45:47 +0300

    add code

--- task40-47/script.js
+++ task40-47/script.js
@@ -1,4 +1,13 @@
 let myFriends = ["Ahmed", "Elham", "Osama", "Gamal"];
 let num = 3;
 
+// Method 1
 console.log(myFriends.slice(0, num));
+
+// Method 2
+// console.log(myFriends.splice(0, num));
+
+
+let friends = ["Ahmed", "Eman", "Osama", "Gamal"];
+
+console.log(myFriends.splice(1,2));
